8c21db27c036cf6871a53850b4af52368ee40f05
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-07-28 22:43:25 -0500

    order class

---
+++ src/main/java/com/project/ecommerce/model/Order.java
@@ -0,0 +1,19 @@
+package com.project.ecommerce.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dateCreated;
+
+    private String status;
+}
