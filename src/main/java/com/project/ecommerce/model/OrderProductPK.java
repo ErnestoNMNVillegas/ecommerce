a82db10fb86dee4f719ce5d22057320cdaf7846b
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-08-03 22:36:51 -0500

    order product pk

---
+++ src/main/java/com/project/ecommerce/model/OrderProductPK.java
@@ -0,0 +1,26 @@
+package com.project.ecommerce.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+
+@Embeddable
+public class OrderProductPK implements Serializable {
+
+    @JsonBackReference
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    // standard getters and setters
+
+    // hashcode() and equals() methods
+}
