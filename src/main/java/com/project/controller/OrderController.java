5ea9d4304cd39e01665a9d7f84771612bcd65d71
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-08-09 22:41:15 -0500

    OrderController

---
+++ src/main/java/com/project/controller/OrderController.java
@@ -0,0 +1,19 @@
+package com.project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    ProductService productService;
+    OrderService orderService;
+    OrderProductService orderProductService;
+
+    public OrderController(ProductService productService, OrderService orderService, OrderProductService orderProductService) {
+        this.productService = productService;
+        this.orderService = orderService;
+        this.orderProductService = orderProductService;
+    }
+}
